87923b66832539a7ece8c682f778317e94686883
Alexandr Demidenko <148003381+soManyVoices@users.noreply.github.com>
2024-06-14 15:10:32 +0300

    Add files via upload

---
+++ Lab9Dem/Lab9Dem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "C:\\Users\\Alexander\\Desktop\\С#\\Lab9Dem\\Lab9Dem\\bin\\Debug\\example.txt";
+        Regex.Replace(File.ReadAllText(path), @"\W+", " ")
+                         .ToLower()
+                         .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                         .Where(word => word.Length >= 4)
+                         .ToList()
+                         .ForEach(Console.WriteLine);
+        Console.WriteLine("Нажмите для выхода");
+        Console.ReadKey();
+    }
+}
